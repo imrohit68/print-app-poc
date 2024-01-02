d5662599b662cf8423cc90814a6843fc7d2b507f
Rohit Singh <sirohit328@gmail.com>
2024-01-02 17:45:34 +0530

    Document and ppt page count page improved

--- src/main/java/com/example/PrintAppPOC/Services/ServiceImpl/DocumentHandlingImpl.java
+++ src/main/java/com/example/PrintAppPOC/Services/ServiceImpl/DocumentHandlingImpl.java
@@ -1,7 +1,9 @@
 package com.example.PrintAppPOC.Services.ServiceImpl;
 
-import com.aspose.words.Document;
 import com.example.PrintAppPOC.Services.DocumentHandlingService;
+import org.apache.poi.sl.usermodel.SlideShow;
+import org.apache.poi.sl.usermodel.SlideShowFactory;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 @Service
@@ -9,8 +11,17 @@ public class DocumentHandlingImpl implements DocumentHandlingService {
     @Override
     public int getPageCount(MultipartFile file) {
         try {
-            Document document = new Document(file.getInputStream());
-            return document.getPageCount();
+            String fileType = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf('.')+1);
+            if(fileType.equals("docx")){
+                XWPFDocument document = new XWPFDocument(file.getInputStream());
+                return document.getProperties().getExtendedProperties().getPages();
+            } else if (fileType.equals("pptx")) {
+                SlideShow slideShow = SlideShowFactory.create(file.getInputStream());
+                return slideShow.getSlides().size();
+            }
+            else {
+                throw new RuntimeException();
+            }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
