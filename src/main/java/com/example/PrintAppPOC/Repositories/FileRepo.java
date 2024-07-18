bb5a6c336c262fe9cda0c7c31f9d37713e5eb91b
Rohit Singh <sirohit328@gmail.com>
2024-07-18 12:14:30 +0530

    push

--- src/main/java/com/example/PrintAppPOC/Repositories/FileRepo.java
+++ src/main/java/com/example/PrintAppPOC/Repositories/FileRepo.java
@@ -3,5 +3,7 @@ package com.example.PrintAppPOC.Repositories;
 import com.example.PrintAppPOC.Entities.Files;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.io.File;
+
 public interface FileRepo extends JpaRepository<Files,String> {
 }
